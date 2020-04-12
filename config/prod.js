3f29c1fe45ca60a678a6597cc94466587061482e
itechdom <oalghnmi@gmail.com>
2020-04-12 00:55:51 -0400

    wikipedia

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,14 @@ import { remote } from "./hosts";
 const config = {
   SERVER: {
     host: remote,
-    port: ""
-  }
+    port: "",
+    wikipedia: {
+      host: "https://en.wikipedia.org/w/api.php",
+      port: "",
+    },
+    wikimedia: {
+      host: "https://commons.wikimedia.org/w/api.php",
+    },
+  },
 };
 export default config;
