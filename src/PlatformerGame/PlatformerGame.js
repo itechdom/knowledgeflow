9bfa140619f8b5a7c56e9a05b316d99023b12873
itechdom <oalghnmi@gmail.com>
2020-05-01 21:05:26 -0400

    clean up

---
+++ src/PlatformerGame/PlatformerGame.js
@@ -0,0 +1,372 @@
+import React from "react";
+import {
+  Grid,
+  Paper,
+  Divider,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Tooltip,
+  Icon,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import TextField from "../orbital-templates/Material/_shared/Forms/Inputs/Forms.TextFieldInput";
+let fpsInterval = 1000 / 60,
+  then = Date.now();
+const animate = (onDraw) => {
+  // request another frame
+  requestAnimationFrame(animate.bind(null, onDraw));
+  // calc elapsed time since last loop
+  let now = Date.now();
+  let elapsed = now - then;
+
+  // if enough time has elapsed, draw the next frame
+  if (elapsed > fpsInterval) {
+    // Get ready for next frame by setting then=now, but also adjust for your
+    // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+    then = now - (elapsed % fpsInterval);
+    onDraw();
+  }
+};
+const renderDialog = ({ title, message, yes, no, onYes, onNo, extra }) => {
+  return (
+    <Dialog open={true} onClose={onNo} aria-labelledby="form-dialog-title">
+      <Grid container alignItems={"flex-end"} justify={"flex-end"}>
+        <Grid item>
+          <Button onClick={onNo}>
+            <Icon>close</Icon>
+          </Button>
+        </Grid>
+      </Grid>
+      <DialogTitle id="form-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+        {extra ? extra : <></>}
+      </DialogContent>
+      <DialogActions>
+        {yes && (
+          <Button onClick={onYes} variant="contained" color="secondary">
+            {yes}
+          </Button>
+        )}
+        {no && (
+          <Button onClick={onNo} variant="outlined" color="primary">
+            {no}
+          </Button>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+};
+export const Game = ({
+  grid,
+  selectGrid,
+  phase,
+  currentPlayer,
+  unSelectAll,
+}) => {
+  const [paused, setPaused] = React.useState(true);
+  const [numberDialog, setNumberDialog] = React.useState();
+  const [currentNumber, setCurrentNumber] = React.useState(0);
+  const [currentLimit, setCurrentLimit] = React.useState(90);
+  const renderNumberDialog = () => {
+    let [i, j] = numberDialog;
+    let tile = grid[i][j];
+    return renderDialog({
+      title: "How many aliens do you want to move",
+      onYes: () => {
+        setNumberDialog(null);
+        selectGrid(parseInt(i), parseInt(j), parseInt(currentNumber));
+      },
+      onNo: () => setNumberDialog(null),
+      yes: "Confirm",
+      no: "Cancel",
+      extra: (
+        <TextField
+          type="number"
+          field={{ name: "alien-count" }}
+          setFieldValue={(key, value) => {
+            setCurrentNumber(value);
+          }}
+          value={currentNumber}
+          max={currentLimit}
+          min={i}
+          standAlone={true}
+        />
+      ),
+    });
+  };
+  const handleClick = (ev) => {
+    let pos = ev.target.dataset.id;
+    if (pos) {
+      let arr = pos.split("-");
+      let [i, j] = arr;
+      let tile = grid[i][j];
+      //display dialog to get the number of aliens moved
+      tile.guide
+        ? setNumberDialog(arr)
+        : selectGrid(parseInt(arr[0]), parseInt(arr[1]));
+      return;
+    }
+  };
+  React.useEffect(() => {
+    // animate(() => {
+    //   console.log("ANIMATE");
+    // });
+    console.log("each group of players can move only once");
+    console.log(
+      "when a new phase is on, display a dialog that automatically disappears explaining what the phase is"
+    );
+  }, [phase]);
+  return (
+    <Grid
+      className="game"
+      container
+      style={{
+        width: "100%",
+        minWidth: "800px",
+        overflow: "scroll",
+        marginLeft: "auto",
+        marginRight: "auto",
+        backgroundColor: "#8BE1EB",
+      }}
+      onClick={(ev) => {
+        handleClick(ev);
+      }}
+    >
+      {numberDialog && renderNumberDialog()}
+      <Dialog className="game" open={paused} onClose={() => setPaused(false)}>
+        <Grid justify="center" container>
+          <Grid item>
+            <Paper
+              style={{
+                padding: "2px",
+                color: "white",
+                minWidth: "400px",
+                textShadow: "black 0px 1px 1px",
+                zIndex: 400,
+                textAlign: "center",
+                backgroundColor: "#A681B5",
+                backgroundImage:
+                  "repeating-linear-gradient(45deg, transparent, transparent 35px, rgba(255,255,255,.5) 35px, rgba(255,255,255,.5) 70px)",
+              }}
+            >
+              <h1>Paused!</h1>
+              <Divider></Divider>
+              <h2>Resume</h2>
+              <Divider></Divider>
+              <h2>Help</h2>
+              <Divider></Divider>
+              <h2>Restart</h2>
+              <Divider></Divider>
+              <h2>Quit</h2>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Dialog>
+      <Grid
+        style={{
+          padding: "2px",
+          color: "white",
+          textShadow: "black 0px 1px 1px",
+          zIndex: 400,
+          textAlign: "center",
+        }}
+        direction="column"
+        justify="center"
+        container
+      >
+        <Grid item>
+          <h1>Knowledge Board !</h1>
+        </Grid>
+        <Grid container justify="center">
+          {currentPlayer === 0 && (
+            <Grid item>
+              <h1>
+                Player <b>1</b> -
+              </h1>
+            </Grid>
+          )}
+          {currentPlayer === 1 && (
+            <Grid item>
+              <h1>
+                Player <b>2</b> -
+              </h1>
+            </Grid>
+          )}
+          {phase === 0 && (
+            <Grid item>
+              <h1>Move</h1>
+            </Grid>
+          )}
+          {phase === 1 && (
+            <Grid item>
+              <h1 style={{ color: "#E97B33" }}>Attack</h1>
+            </Grid>
+          )}
+          {phase === 2 && (
+            <Grid item>
+              <h1 style={{ color: "#8DC434" }}>Rienforce</h1>
+            </Grid>
+          )}
+          <Typography>
+            {JSON.stringify(grid.map((g) => g.map((gr) => `${gr.count}`)))}
+          </Typography>
+          <div style={{ marginBottom: "3em" }}></div>
+          {/* <Grid item>
+            <Grid container justify="center">
+              <Button className="game_button" variant="outlined">
+                <h1 onClick={() => endTurn()} className="game">
+                  End Turn!
+                </h1>
+              </Button>
+            </Grid>
+          </Grid> */}
+        </Grid>
+      </Grid>
+      <Grid container justify="center" direction="row">
+        {grid.length > 0 ? (
+          grid.map((g, i) => (
+            <Grid
+              container
+              key={`${i}-container`}
+              style={{
+                position: "relative",
+                bottom: `${i + 1}px`,
+                left: (i + 1) % 2 === 0 ? "33px" : "0px",
+              }}
+              justify="center"
+            >
+              {g.map((gr, j) => (
+                <Tooltip
+                  key={`${i}-${j}-tooltip`}
+                  title={
+                    gr.type === "character"
+                      ? j === 0
+                        ? "This is you!"
+                        : "Player2"
+                      : ""
+                  }
+                >
+                  <Grid
+                    style={{
+                      width: "70px",
+                      height: "85px",
+                      position: "relative",
+                    }}
+                    item
+                  >
+                    {gr.environment &&
+                      gr.environment.map((env, index) => (
+                        <img
+                          data-id={`${i}-${j}-character`}
+                          id={`${i}-${j}-environment`}
+                          key={`${i}-${j}-environment`}
+                          style={{
+                            position: "absolute",
+                            left: `48px`,
+                            bottom: "33px",
+                            display: "inline",
+                            height: gr.type === "background" ? "auto" : "87px",
+                            zIndex: gr.type === "background" ? 150 : 300,
+                            opacity: gr.selected ? 1 : 0.25,
+                          }}
+                          src={env}
+                        />
+                      ))}
+                    {gr.characters &&
+                      gr.characters.map((env, index) => (
+                        <img
+                          data-id={`${i}-${j}-character`}
+                          id={`${i}-${j}-character`}
+                          key={`${i}-${j}-character`}
+                          className={"game_character"}
+                          style={{
+                            position: "absolute",
+                            left: "0px",
+                            bottom: `${24}px`,
+                            display: "inline",
+                            height: gr.type === "background" ? "auto" : "87px",
+                            zIndex: gr.type === "background" ? 150 : 300,
+                            visibility:
+                              !gr.display && gr.selected ? "" : "hidden",
+                          }}
+                          src={env}
+                        />
+                      ))}
+                    {gr.type === "character" && (
+                      <span
+                        data-id={`${i}-${j}-count`}
+                        id={`${i}-${j}-count`}
+                        key={`${i}-${j}-count`}
+                        style={{
+                          padding: "2px",
+                          color: "white",
+                          textShadow: "black 0px 1px 1px",
+                          fontSize: 18,
+                          position: "absolute",
+                          left: "48px",
+                          bottom: "84px",
+                          display: "inline-block",
+                          zIndex: 400,
+                        }}
+                      >
+                        {gr.count}
+                      </span>
+                    )}
+                    <img
+                      data-id={`${i}-${j}-title`}
+                      id={`${i}-${j}-tile`}
+                      key={`${i}-${j}-tile`}
+                      style={{
+                        padding: "3px 5px",
+                        position: "absolute",
+                        left: 0,
+                        boxShadow: gr.guide ? "green 0px 0px 15px" : "",
+                        borderRadius: gr.guide ? "50px" : "",
+                        zIndex: gr.type === "background" ? 100 : 200,
+                        opacity: gr.selected ? 1 : 0.25,
+                      }}
+                      src={gr.tile}
+                      onClick={(ev) => {}}
+                    />
+                    {gr.guide ? (
+                      <span
+                        data-id={`${i}-${j}-guide`}
+                        id={`${i}-${j}-guide`}
+                        key={`${i}-${j}-guide`}
+                        onClick={(ev) => {
+                          ev.stopPropagation();
+                          handleClick(ev);
+                        }}
+                        style={{
+                          position: "absolute",
+                          top: 24,
+                          left: 12,
+                          zIndex: 999,
+                          color: "white",
+                          textShadow: "black 0px 1px 1px",
+                          cursor: gr.guide ? "pointer" : "inherit",
+                        }}
+                      >
+                        here
+                      </span>
+                    ) : (
+                      <></>
+                    )}
+                  </Grid>
+                </Tooltip>
+              ))}
+            </Grid>
+          ))
+        ) : (
+          <></>
+        )}
+      </Grid>
+    </Grid>
+  );
+};
+export default Game;
---
+++ src/PlatformerGame/PlatformerGame.state.js
@@ -0,0 +1,274 @@
+import React from "react";
+import { getRandomInt } from "./utils";
+const assetLocation =
+  "http://knowledgeflow.markab.io.s3-website-us-east-1.amazonaws.com/";
+const tiles = [
+  "Grass",
+  "Lava",
+  "Magic",
+  // "Dirt",
+  "Sand",
+  "Snow",
+  // "Stone",
+  // "Water",
+];
+export const GameState = ({ children, knowledge }) => {
+  const [grid, setGrid] = React.useState([]);
+  const [phase, setPhase] = React.useState(0);
+  const [currentTile, setCurrentTile] = React.useState([0, 0]);
+  const [currentPlayer, setCurrentPlayer] = React.useState(0);
+  const [mode, setMode] = React.useState("free");
+  const getRandomCharacter = () => {
+    const options = ["Beige", "Blue", "Green", "Pink", "Yellow"];
+    const randomCharacter = options[getRandomInt(0, options.length - 1)];
+    return `${assetLocation}game/hexagonTiles/Tiles/alien${randomCharacter}.png`;
+  };
+  const getCurrentPlayer = () => {
+    let pos1, pos2;
+    let final = grid
+      .map((g, i) => {
+        let players = g.filter((gr, j) => {
+          if (gr.type === "character" && gr.player === currentPlayer) {
+            pos1 = i;
+            pos2 = j;
+            return true;
+          }
+          return false;
+        });
+        return players;
+      })
+      .filter((g) => g.length > 0);
+    return { ...final[0][0], i: pos1, j: pos2 };
+  };
+  const getATile = (tile) => {
+    let suffix = "_full";
+    if (tile === "Water") {
+      suffix = "_shadow";
+    }
+    return `${assetLocation}game/hexagonTiles/Tiles/tile${tile}${suffix}.png`;
+  };
+  const getTile = (tile, selected) => {
+    const suffix = "";
+    if (tile) {
+      if (selected) {
+        return `${tile.tile.replace(suffix, "")}`;
+      }
+      if (tile.tile.indexOf(suffix) === -1) {
+        return `${tile.tile.replace(".png", "")}${suffix}.png`;
+      }
+      return tile.tile;
+    }
+    return `${assetLocation}game/hexagonTiles/Tiles/tile${
+      tiles[getRandomInt(0, tiles.length - 1)]
+    }${suffix}.png`;
+  };
+  const getRandomTree = (season) => {
+    const trees = ["Blue", "Green"];
+    const suffix = "_low";
+    const randomTree = `${assetLocation}game/hexagonTiles/Tiles/tree${
+      trees[getRandomInt(0, trees.length - 1)]
+    }${suffix}.png`;
+    return randomTree;
+  };
+  const getRandomRock = (season) => {
+    const randomTree = `${assetLocation}game/hexagonTiles/Tiles/smallRockGrass.png`;
+    console.log(randomTree);
+    return randomTree;
+  };
+  const getBackgroundEnvironment = () => {
+    return getRandomInt(0, 1) < 1
+      ? `${assetLocation}game/hexagonTiles/Tiles/waveWater.png`
+      : ``;
+  };
+  const getBackgroundTile = () => {
+    const tile = {
+      name: `Water`,
+      tile: getATile("Water"),
+      selected: true,
+      type: "background",
+      environment: [getBackgroundEnvironment()],
+    };
+    return tile;
+  };
+
+  //this is just a fun test, using conway's game of life
+  const simulate = () => {};
+
+  const initGrid = () => {
+    for (let i = 0; i < 9; i++) {
+      if (!grid[i]) grid[i] = [];
+      for (let j = 0; j < 10; j++) {
+        grid[i][j] = {
+          // name: `${i}x${j}`,
+          tile: getTile(),
+          environment: getRandomInt(1, 2) === 2 ? [getRandomTree()] : [],
+          selected: false,
+          count: 0,
+        };
+      }
+    }
+
+    //ADDING RANDOM CHRACTERS
+    grid.map((g, i) => {
+      g.map((gr, j) => {
+        if (
+          (i === 0 && j === 0) ||
+          (i === grid.length - 1 && j === g.length - 1)
+        ) {
+          grid[i][j] = {
+            name: `x90`,
+            tile: getTile(),
+            player: i === 0 ? 0 : 1,
+            environment: [],
+            characters: [getRandomCharacter()],
+            moved: false,
+            count: 90,
+            type: "character",
+            selected: true,
+          };
+        }
+      });
+    });
+    setCurrentTile({ ...grid[0][0], i: 0, j: 0 });
+    return setGrid(grid);
+  };
+  const selectSurrondingGrids = (i, j) => {
+    //show nearby tiles that the user can move to
+    let adj1 = grid[i] && grid[i][j] && grid[i][j];
+    if (adj1) {
+      grid[i][j] = {
+        ...adj1,
+        guide: !adj1.guide,
+      };
+    }
+    setGrid([...grid]);
+  };
+  const selectGrid = (i, j, count) => {
+    //you can move to this tile because it has a guide on it
+    //TODO: move this as a side effect
+    let isOddRow = i % 2 !== 0 ? true : false;
+    if (grid[i][j].guide) {
+      console.log("MODE", mode);
+      //move player to that grid
+      let playerTile = grid[currentTile.i][currentTile.j];
+      let toTile = grid[i][j];
+      let playerTileCount = playerTile.count - count;
+      let toTileCount = toTile.count + count;
+      if (toTileCount > playerTile.count) {
+        toTileCount = playerTile.count;
+      }
+      grid[currentTile.i][currentTile.j] = {
+        ...toTile,
+        environment:
+          playerTile.environment.length === 0 ? [] : playerTile.environment,
+        tile: playerTile.tile,
+        count: playerTileCount <= 0 ? 0 : playerTileCount,
+        type: "character",
+        characters: playerTileCount <= 0 ? [] : playerTile.characters,
+        player: playerTile.player,
+        selected: true,
+      };
+      grid[i][j] = {
+        ...playerTile,
+        environment: toTile.environment.length === 0 ? [] : toTile.environment,
+        tile: toTile.tile,
+        count: toTileCount <= 0 ? 0 : toTileCount,
+        type: "character",
+        characters: toTileCount <= 0 ? [] : playerTile.characters,
+        player: playerTile.player,
+        selected: true,
+      };
+      setMode("free");
+      setCurrentTile({ ...grid[i][j], i, j });
+      return unSelectAll([...grid]);
+    }
+    //you want to move a character
+    if (phase === 0) {
+      if (grid[i][j].type === "character") {
+        if (grid[i][j].player === currentPlayer) {
+          if (mode === "guide") {
+            setMode("free");
+            return unSelectAll([...grid]);
+          }
+          setMode("guide");
+          if (isOddRow) {
+            selectSurrondingGrids(i, j + 1);
+            selectSurrondingGrids(i, j - 1);
+            selectSurrondingGrids(i + 1, j);
+            selectSurrondingGrids(i + 1, j + 1);
+            selectSurrondingGrids(i - 1, j);
+            selectSurrondingGrids(i - 1, j + 1);
+          } else {
+            selectSurrondingGrids(i, j - 1);
+            selectSurrondingGrids(i, j + 1);
+            selectSurrondingGrids(i - 1, j - 1);
+            selectSurrondingGrids(i - 1, j);
+            selectSurrondingGrids(i + 1, j - 1);
+            selectSurrondingGrids(i + 1, j);
+          }
+        }
+      }
+      return setCurrentTile({ ...grid[i][j], i, j });
+    }
+    unSelectAll([...grid]);
+    return setCurrentTile({ ...grid[i][j], i, j });
+  };
+  const updateGrid = (position1, position2, data) => {
+    setGrid(
+      grid.map((g, i) =>
+        i === position1 ? g.map((gr, j) => (j === position2 ? data : gr)) : g
+      )
+    );
+  };
+  const unSelectAll = (grid) => {
+    const newGrid = grid.map((g, i) => {
+      return g.map((gr, j) => {
+        return {
+          ...gr,
+          guide: false,
+        };
+      });
+    });
+    setGrid([...newGrid]);
+    return newGrid;
+  };
+  const RollADice = () => {
+    let roll = getRandomInt(1, 6);
+    return roll;
+  };
+  const endTurn = () => {
+    const nextPhase = phase + 1;
+    //move
+    if (phase === 0) {
+    }
+    //attack
+    else if (phase === 1) {
+    }
+    //rienforce
+    else if (phase === 2) {
+      setCurrentPlayer(currentPlayer === 1 ? 0 : 1);
+      setPhase(0);
+      return;
+    }
+    return setPhase(nextPhase);
+  };
+  React.useEffect(() => {
+    initGrid();
+    setPhase(0);
+    setCurrentPlayer(0);
+  }, []);
+  console.log("rerender");
+  //knowledgeflow.markab.io
+  const childrenWithProps = React.Children.map(children, (child) => {
+    return React.cloneElement(child, {
+      grid: grid,
+      updateGrid: updateGrid,
+      unSelectAll: unSelectAll,
+      selectGrid: selectGrid,
+      RollADice: RollADice,
+      phase,
+      currentPlayer,
+    });
+  });
+  return <>{childrenWithProps}</>;
+};
