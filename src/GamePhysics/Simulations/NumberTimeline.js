e271cb4e6fc2105d1ba5d2983ccff1d52f536598
itechdom <oalghnmi@gmail.com>
2020-05-18 19:33:18 +0700

    render numbers

--- src/GamePhysics/Simulations/NumberTimeline.js
+++ src/GamePhysics/Simulations/NumberTimeline.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Matter from "matter-js";
 import { Grid } from "@material-ui/core";
-const NumberTimeline = ({ initMatter }) => {
+const NumberTimeline = ({ initMatter, x, y, ...rest }) => {
+  console.log(rest, "Position");
   const [myEngine, setMyEngine] = React.useState();
   const init = (options) => {
     const { engine, mouse, render } = initMatter(
@@ -12,39 +13,52 @@ const NumberTimeline = ({ initMatter }) => {
     let stack = Matter.Composites.stack(
       0,
       250,
-      10,
-      1,
       100,
+      1,
+      10,
       0,
       (x, y, column, row, lastBody, i) => {
         //restitution is the ratio of end velocity to beginning velocity
-        let circle1 = Matter.Bodies.circle(x, y, 10, {
+        let circle1 = Matter.Bodies.circle(x, y, 25, {
           restitution: 0,
+          isStatic: true,
           mass: 0,
         });
+        circle1.number = i;
         // let chain = Matter.Composites.chain(stack, 0.5, 0, -0.5, 0);
 
         return circle1;
       }
     );
     stack.bodies.map((bod, i) => {
-      Matter.Composite.add(
-        stack,
-        Matter.Constraint.create({
-          bodyA: bod,
-          bodyB: stack.bodies[i - 1] ? stack.bodies[i - 1] : null,
-          pointB: stack.bodies[i - 1] ? null : 0,
-          stiffness: 0,
-        })
-      );
+      stack.bodies[i - 1] &&
+        Matter.Composite.add(
+          stack,
+          Matter.Constraint.create({
+            bodyA: bod,
+            bodyB: stack.bodies[i - 1],
+            stiffness: 0,
+          })
+        );
+    });
+    Matter.Events.on(engine, "afterUpdate", () => {
+      stack.bodies.map((bod) => {
+        if (bod.number) {
+          let { x, y } = bod.position;
+          render.context.fillStyle = "#FFF";
+          render.context.fillText(`${bod.number}`, x + 5, y);
+        }
+      });
     });
     let circles = [stack];
-    Matter.World.add(engine.world, circles);
+    let c = Matter.Bodies.circle(400, 100, 50);
+    Matter.World.add(engine.world, [...circles, c]);
     setMyEngine(engine);
   };
   React.useEffect(() => {
-    init({ wireframes: true, background: "#FFF", showAngleIndicator: false });
+    init({ wireframes: false, background: "#FFF", showAngleIndicator: false });
   }, []);
+  React.useEffect(() => {});
   return (
     <Grid item style={{ marginTop: "10px" }}>
       <Grid
